9a7d113d5699712d10de834b7e5b175029bc3881
Pratyush Kumar Swain <pratyushkumarswain101@gmail.com>
2025-08-10 03:13:32 +0530

    Store hashed OTP and Implement verify OTP logic

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -12,16 +12,17 @@ module.exports.sendOtp = async (req, res) => {
 
     try {
         const otp = Math.floor(100000 + Math.random() * 900000).toString();
+        const hashedOTP = await bcrypt.hash(otp, 10);
         const otpExpiry = Date.now() + 5 * 60 * 1000; // 5 minures
 
         let user = await userModel.findOne({ email });
 
         if (!user) {
-            user = await userModel.create({ email, otp, otpExpiry });
+            user = await userModel.create({ email, otp: hashedOTP, otpExpiry });
         } else {
             await userModel.findOneAndUpdate(
                 { email },
-                { otp, otpExpiry }
+                { otp: hashedOTP, otpExpiry }
             );
         }
 
@@ -32,5 +33,37 @@ module.exports.sendOtp = async (req, res) => {
     } catch (error) {
         console.error('Error sending OTP', error);
         res.status(500).json({ message: 'Failed to send OTP' });
+    }
+};
+
+// 2. Verify OTP
+module.exports.verifyOtp = async (req, res) => {
+    const { email, otp } = req.body;
+
+    try {
+        const user = await userModel.findOne({ email });
+
+        if (!user) {
+            return res.status(404).json({ message: 'User not found' });
+        }
+
+        if (user.otpExpiry < Date.now()) {
+            return res.status(400).json({ message: 'OTP expired' });
+        }
+
+        const isMatch = await bcrypt.compare(otp, user.otp);
+        if (!isMatch) {
+            return res.status(400).json({ message: 'Invalid OTP' });
+        }
+
+        await userModel.findOneAndUpdate(
+            { email },
+            { isVerified: true, otp: undefined, otpExpiry: undefined }
+        );
+
+        res.status(200).json({ message: 'OTP verified successfully' });
+    } catch (error) {
+        console.error('Error: ', error);
+        res.status(500).json({ message: 'Failed to verify OTP' });
     }
 };
--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -2,22 +2,19 @@ const express = require('express');
 const { body, validationResult } = require('express-validator');
 const userController = require('../controllers/userController');
 const router = express.Router();
+const validationMiddleware = require('../utils/validationMiddleware');
 
 router.post('/register/send-otp',
     body('email').trim().isEmail().withMessage('A valid email is required'),
-    (req, res, next) => {
-        const errors = validationResult(req);
-
-        if (!errors.isEmpty()) {
-            return res.status(400).json({
-                errors: errors.array(),
-                message: 'Invalid data'
-            });
-        }
-
-        next()
-    },
+    validationMiddleware(validationResult),
     userController.sendOtp
 );
+
+router.post('/register/verify-otp',
+    body('email').trim().isEmail().withMessage('A valid email is required'),
+    body('otp').trim().isLength({ min: 6, max: 6 }),
+    validationMiddleware(validationResult),
+    userController.verifyOtp
+)
 
 module.exports.router = router;
