701f1cea111f035aec915cee681c9f59b53dc85c
Pratyush Kumar Swain <pratyushkumarswain101@gmail.com>
2025-08-13 10:46:41 +0530

    Implement AOS

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,18 @@
+import AOS from 'aos';
+import 'aos/dist/aos.css';
 import './App.css'
+import { useEffect } from 'react';
 
 const App = () => {
+
+  useEffect(() => {
+    AOS.init({
+      duration: 1000,
+      once: true,
+      offset: 80,
+      easing: 'ease-in-out'
+    });
+  }, []);
 
   return (
     <h2 className='text-center'>Welcome to MediBookQ</h2>
