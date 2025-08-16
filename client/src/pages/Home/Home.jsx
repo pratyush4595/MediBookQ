f7382814e80bb012e75ad82f5cbbdc6abfcaeae0
Pratyush Kumar Swain <pratyushkumarswain101@gmail.com>
2025-08-17 03:09:14 +0530

    Implement coming soon

--- client/src/pages/Home/Home.jsx
+++ client/src/pages/Home/Home.jsx
@@ -1,6 +1,16 @@
+import './Home.css';
+
 const Home = () => {
     return (
-        <h2>Welcome to Home Page</h2>
+        <>
+            {/*-- Coming Soon Section -*/}
+            <div className="container">
+                <div className="coming-soon bg-body-tertiary">
+                    <h1>Coming Soon</h1>
+                    <p>I'm working on something amazing. Check back soon!</p>
+                </div>
+            </div>
+        </>
     );
 }
 
