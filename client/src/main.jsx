e02da49a4c231c8eba5418146c3f04aebfafc2cc
Pratyush Kumar Swain <pratyushkumarswain101@gmail.com>
2025-08-19 19:35:51 +0530

    Bug fixes

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,6 +1,6 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import Provider from 'react-redux';
+import { Provider } from 'react-redux';
 
 import './index.css'
 import './styles/theme.css';
